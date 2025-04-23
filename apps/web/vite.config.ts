e9f81aab9982da7fc25074fc260e20214fc65629
Zaberahmed <zaber0305@gmail.com>
2025-04-23 06:05:49 +0600

    Add: web folder for client side

---
+++ apps/web/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import deno from "@deno/vite-plugin";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [deno(), react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
+  },
+});
