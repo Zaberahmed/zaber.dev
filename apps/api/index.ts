66579fc09a24ac87fd1dafa1050599c2abfb40b4
Zaberahmed <zaber0305@gmail.com>
2025-04-27 00:10:30 +0600

    Build: added env files

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,7 +1,9 @@
+import "https://deno.land/std@0.220.1/dotenv/load.ts";
 import { createServer } from "./server.ts";
 
 const server = createServer();
+const port = Deno.env.get("API_LOCAL_PORT") || 3000;
 
-server.listen(3000);
-
-console.log("🚀 Server ready at http://localhost:3000");
+server.listen(port, () => {
+  console.log(`🚀 Server is running on http://localhost:${port}`);
+});
--- apps/api/server.ts
+++ apps/api/server.ts
@@ -1,3 +1,4 @@
+import "https://deno.land/std@0.220.1/dotenv/load.ts";
 import { createHTTPServer } from "@trpc/server/adapters/standalone";
 import { appRouter } from "./trpc/routers/index.ts";
 
@@ -6,10 +7,14 @@ export const createServer = () => {
    * Create HTTP server
    * @see https://trpc.io/docs/adapters/standalone
    */
+  const port = Deno.env.get("WEB_LOCAL_PORT") || 5173;
+  const web_url = Deno.env.get("WEB_URL");
+  const allowedOrigin = web_url || `http://127.0.0.1:${port}`;
+
   return createHTTPServer({
     router: appRouter,
     createContext({ req, res }) {
-      res.setHeader("Access-Control-Allow-Origin", "https://zaber.deno.dev");
+      res.setHeader("Access-Control-Allow-Origin", `${allowedOrigin}`);
       res.setHeader("Access-Control-Request-Method", "*");
       res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST");
       res.setHeader("Access-Control-Allow-Headers", "*");
