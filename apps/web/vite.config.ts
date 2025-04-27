3b117b986191e87e09b1fdf05e796c398071dc3e
Zaberahmed <zaber0305@gmail.com>
2025-04-28 00:54:38 +0600

    Chore: made some tweaks in deploy yml

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,28 +1,34 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
+import process from "node:process";
 import deno from "@deno/vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [deno(), react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: Deno.env.get("VITE_API_URL") || "http://localhost:6200",
-        changeOrigin: true,
+export default () => {
+  // Load environment variables from .env file
+  process.env = {
+    ...process.env,
+    ...loadEnv("", process.cwd() + "../../", "VITE_"),
+  };
+  console.log("VITE_API_URL", process.env.VITE_API_URL);
+  return defineConfig({
+    plugins: [deno(), react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: Deno.env.get("VITE_API_URL") || "http://localhost:6200",
+          changeOrigin: true,
+        },
       },
     },
-  },
 
-  envDir: "../../",
-  envPrefix: "VITE_",
-
-  build: {
-    // Generate static assets in the dist folder
-    outDir: "dist",
-    // Clean the output directory before building
-    emptyOutDir: true,
-    // Optimize asset size
-    minify: true,
-  },
-});
+    build: {
+      // Generate static assets in the dist folder
+      outDir: "dist",
+      // Clean the output directory before building
+      emptyOutDir: true,
+      // Optimize asset size
+      minify: true,
+    },
+  });
+};
