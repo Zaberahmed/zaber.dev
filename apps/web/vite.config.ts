ead0679d00f0e27dc6e7392c650cda7ada7c0603
Zaberahmed <zaber0305@gmail.com>
2025-04-29 04:57:27 +0600

    Chore: modified vite config back

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,7 +2,6 @@ import deno from "@deno/vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 import path from "node:path";
 import { defineConfig, loadEnv } from "vite";
-import "https://deno.land/std@0.220.1/dotenv/load.ts";
 
 // https://vite.dev/config/
 export default ({ mode }: { mode: string }) => {
@@ -10,8 +9,7 @@ export default ({ mode }: { mode: string }) => {
   const root = path.resolve(__dirname, "../../");
   const env = loadEnv(mode, root, "");
 
-  const apiUrl =
-    mode === "production" ? Deno.env.get("VITE_API_URL") : env.VITE_API_URL;
+  const apiUrl = env.VITE_API_URL;
 
   return defineConfig({
     plugins: [deno(), react()],
