9be97fac4a8489767ecf946bba0fda8d7f718342
Zaberahmed <zaber0305@gmail.com>
2025-05-09 01:07:26 +0600

    Modify: updated success response messages

--- apps/api/constants/response-messages/success-messages.constant.ts
+++ apps/api/constants/response-messages/success-messages.constant.ts
@@ -6,6 +6,8 @@ const AuthSuccessMessages = {
 
 const UserSuccessMessages = {
   ADMIN_USER_CREATED: "Admin user created successfully.",
+  USER_RETRIEVED: "User retrieved successfully.",
+  USER_ALL_RETRIEVED: "All users retrieved successfully.",
   USER_DELETED: "User deleted successfully.",
   USER_DELETED_ALL: "All users deleted successfully.",
   USER_UPDATED: "User updated successfully.",
