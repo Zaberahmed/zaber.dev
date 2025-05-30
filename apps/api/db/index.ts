97e9a6071427397c6fa8f7512df83f875bc0365f
Sk. Zaber Ahmed <zaber0305@gmail.com>
2025-05-31 03:14:34 +0600

    removed unnecessary comments

--- apps/api/db/index.ts
+++ apps/api/db/index.ts
@@ -7,18 +7,15 @@ import {
 import { handleProcedure } from "../utils/response.ts";
 import * as schema from "./schema.ts";
 
-// Get database connection details from environment variables
 const dbConnectionString = DATABASE_CONNECTION_STRING;
 const isProductionEnv = DEPLOYMENT_ENV === "production";
 
-// Create a postgres connection
 export const client = postgres(dbConnectionString, {
   max: 10, // Maximum number of connections
   connect_timeout: 60, // Timeout for establishing a connection
   ssl: isProductionEnv ? "require" : false, // Use SSL in production
 });
 
-// Function to check database connection
 export function checkDatabaseConnection(
   client: postgres.Sql<Record<PropertyKey, never>>
 ): Promise<boolean> {
@@ -29,5 +26,4 @@ export function checkDatabaseConnection(
   }, "connect to database");
 }
 
-// Create Drizzle ORM instance with our schema
 export const db = drizzle(client, { schema });
