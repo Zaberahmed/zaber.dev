dd145352efb9812747aab48e232e98d556300c4f
Sk. Zaber Ahmed <zaber0305@gmail.com>
2025-05-31 03:02:15 +0600

    conditionally connecting to db with sslmode

--- apps/api/db/index.ts
+++ apps/api/db/index.ts
@@ -1,16 +1,21 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-import { DATABASE_CONNECTION_STRING } from "../constants/global.constant.ts";
+import {
+  DATABASE_CONNECTION_STRING,
+  DEPLOYMENT_ENV,
+} from "../constants/global.constant.ts";
 import { handleProcedure } from "../utils/response.ts";
 import * as schema from "./schema.ts";
 
 // Get database connection details from environment variables
 const dbConnectionString = DATABASE_CONNECTION_STRING;
+const isProductionEnv = DEPLOYMENT_ENV === "production";
 
 // Create a postgres connection
 const client = postgres(dbConnectionString, {
   max: 10, // Maximum number of connections
   connect_timeout: 60, // Timeout for establishing a connection
+  ssl: isProductionEnv ? "require" : false, // Use SSL in production
 });
 
 // Function to check database connection
