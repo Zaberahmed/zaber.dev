9443a83918724a65deda82a79358f4a676158f0b
Sk. Zaber Ahmed <zaber0305@gmail.com>
2025-05-31 03:16:00 +0600

    simplified server connection logic

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,13 +1,14 @@
 import "https://deno.land/std@0.220.1/dotenv/load.ts";
 import { createServer } from "./server.ts";
+import { DEPLOYMENT_ENV } from "./constants/global.constant.ts";
 
 const port = Deno.env.get("API_LOCAL_PORT") || 6200;
-const denoDeploymentId = Deno.env.get("DENO_DEPLOYMENT_ID");
+const isProductionEnv = DEPLOYMENT_ENV === "production";
 
 const server = await createServer();
 
 server.listen(port, () => {
-  if (!denoDeploymentId) {
+  if (!isProductionEnv) {
     console.log(`🚀 Server is running on http://localhost:${port}`);
   }
 });
