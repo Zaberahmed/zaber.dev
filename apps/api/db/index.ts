56a409ceba1b86099ddb4166e6bc514414672085
Sk. Zaber Ahmed <zaber0305@gmail.com>
2025-06-01 01:02:35 +0600

    testing new deno-postgres package

--- apps/api/db/index.ts
+++ apps/api/db/index.ts
@@ -6,9 +6,15 @@ import {
 } from "../constants/global.constant.ts";
 import { handleProcedure } from "../utils/response.ts";
 import * as schema from "./schema.ts";
+import { Client } from "jsr:@db/postgres";
 
 const dbConnectionString = DATABASE_CONNECTION_STRING;
 const isProductionEnv = DEPLOYMENT_ENV === "production";
+
+export const newClient = new Client(dbConnectionString);
+
+const res = await newClient.connect();
+console.log("Database connection established:", res);
 
 export const client = postgres(dbConnectionString, {
   max: 10, // Maximum number of connections
@@ -16,12 +22,11 @@ export const client = postgres(dbConnectionString, {
   ssl: isProductionEnv ? { rejectUnauthorized: false } : false,
 });
 
-export function checkDatabaseConnection(
-  client: postgres.Sql<Record<PropertyKey, never>>
-): Promise<boolean> {
+export function checkDatabaseConnection(client: Client): Promise<boolean> {
   return handleProcedure(async () => {
     // Simple query to check if database is accessible
-    await client`SELECT 1`;
+    await client.queryArray`SELECT 1`;
+    console.log("Database connection is healthy");
     return true;
   }, "connect to database");
 }
