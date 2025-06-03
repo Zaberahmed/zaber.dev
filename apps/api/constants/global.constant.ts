aa7272f77062db729e8861f23ccee5024f757898
Sk. Zaber Ahmed <zaber0305@gmail.com>
2025-06-03 20:33:13 +0600

    added new constants

--- apps/api/constants/global.constant.ts
+++ apps/api/constants/global.constant.ts
@@ -1,4 +1,9 @@
 const TimeConstants = {
+  HALF_SECOND: 0.5,
+  ONE_SECOND: 1,
+  FIVE_SECONDS: 5,
+  TEN_SECONDS: 10,
+  THIRTY_SECONDS: 30,
   ONE_MINUTE: 60,
   ONE_HOUR: 60 * 60,
   ONE_DAY: 60 * 60 * 24,
@@ -6,20 +11,21 @@ const TimeConstants = {
   ONE_MONTH: 60 * 60 * 24 * 30,
 } as const;
 
+// Default values
 const ADMIN_DEFAULT_SETUP_KEY = "your-super-secret-setup-key";
-
-const ADMIN_SETUP_KEY =
-  Deno.env.get("ADMIN_SETUP_KEY") || ADMIN_DEFAULT_SETUP_KEY;
-
 const DEFAULT_DATABASE_URL =
   "postgresql://postgres:postgres@localhost:5432/test";
+const DEFAULT_DATABASE_CA_CERT = "";
+const DEFAULT_DEPLOYMENT_ENV = "development";
 
+// Environment variables with fallbacks
+const ADMIN_SETUP_KEY =
+  Deno.env.get("ADMIN_SETUP_KEY") || ADMIN_DEFAULT_SETUP_KEY;
 const DATABASE_CONNECTION_STRING =
   Deno.env.get("DATABASE_URL") || DEFAULT_DATABASE_URL;
-
-const DATABASE_CA_CERT = Deno.env.get("DATABASE_CA_CERT");
-
-const DEPLOYMENT_ENV = Deno.env.get("DEPLOYMENT_ENV") || "development";
+const DATABASE_CA_CERT =
+  Deno.env.get("DATABASE_CA_CERT") || DEFAULT_DATABASE_CA_CERT;
+const DEPLOYMENT_ENV = Deno.env.get("DEPLOYMENT_ENV") || DEFAULT_DEPLOYMENT_ENV;
 
 export {
   TimeConstants,
