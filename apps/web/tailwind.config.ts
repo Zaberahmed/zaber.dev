93c021d0952acf9dcec748d90d7e87d3e8a9165f
Sk. Zaber Ahmed <zaber0305@gmail.com>
2025-07-11 21:24:14 +0600

    converted tailwind and postcss to ts files

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from "tailwindcss";
+
 export default {
   content: [
     "./index.html",
@@ -51,4 +52,4 @@ export default {
     },
   },
   plugins: [],
-};
+} satisfies Config;
