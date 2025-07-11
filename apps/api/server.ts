0d0fcc885e5d1d5353cb2ac143ec7a11d2f0da01
Sk. Zaber Ahmed <zaber0305@gmail.com>
2025-07-11 21:07:52 +0600

    made cors options allow multiple origins

--- apps/api/server.ts
+++ apps/api/server.ts
@@ -5,11 +5,11 @@ import { createContext } from "@trpc/context.ts";
 import { checkDatabaseConnection, pool } from "@db/index.ts";
 
 const port = Deno.env.get("WEB_LOCAL_PORT") || 5173;
-const web_url = Deno.env.get("WEB_URL");
-const allowedOrigin = web_url || `http://127.0.0.1:${port}`;
+const web_url = Deno.env.get("WEB_URL") || `http://localhost:${port}`;
+const allowedOrigins = [web_url, `http://127.0.0.1:${port}`];
 
 const corsOptions = {
-  origin: allowedOrigin,
+  origin: [...allowedOrigins],
   methods: ["GET", "POST", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
