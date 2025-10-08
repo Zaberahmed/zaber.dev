9344d0d6f0235c02b46b7ef41b63f7cb5ce03987
Sk. Zaber Ahmed <zaber0305@gmail.com>
2025-10-08 23:28:30 +0600

    remove: commented out lines

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import RoutesComponent from "@routes/index.ts";
-// import Navbar from "@sections/navbar/index.ts";
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <Navbar /> */}
       <RoutesComponent />
     </BrowserRouter>
   );
