2d2cca028d57fe6d04b2bd0ed40ffa46afea15f3
Sk. Zaber Ahmed <zaber0305@gmail.com>
2026-06-03 18:09:36 +0600

    update: deploy yaml file

--- apps/web/src/configs/trpc.ts
+++ apps/web/src/configs/trpc.ts
@@ -5,11 +5,12 @@ import { httpBatchLink } from "@trpc/client";
 import { createTRPCReact } from "@trpc/react-query";
 
 export const trpc = createTRPCReact<AppRouter>();
+const apiUrl = import.meta.env.VITE_API_URL || "/api";
 
 const trpcClientConfig = {
   links: [
     httpBatchLink({
-      url: `${import.meta.env.VITE_API_URL}`,
+      url: apiUrl,
     }),
   ],
 };
