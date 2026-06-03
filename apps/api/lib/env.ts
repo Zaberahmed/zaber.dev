ee00b0a440766353a04a479a0aab1dd63aa2698d
Sk. Zaber Ahmed <zaber0305@gmail.com>
2026-06-03 17:50:19 +0600

    update: secrets type support for both local and prod

--- apps/api/lib/env.ts
+++ apps/api/lib/env.ts
@@ -1,21 +1,48 @@
-import type { Secret } from "./types.ts";
+import type { DeploymentMode, Env, Secret } from "./types.ts";
 
 function loadEnvVariables() {
   const secrets: Secret[] = [
-    "API_LOCAL_PORT",
+    "DEPLOYMENT_MODE",
     "CORS_ALLOWED_ORIGINS",
     "JWT_SECRET",
     "ADMIN_SETUP_KEY",
     "DATABASE_URL",
   ] as const;
-  for (const secret of secrets) {
-    if (!Deno.env.get(secret)) {
+  const values = secrets.reduce((acc, secret) => {
+    const value = Deno.env.get(secret);
+
+    if (!value) {
       throw new Error(`${secret} not set`);
     }
-  }
-  return secrets.reduce((acc, secret) => {
-    acc[secret] = Deno.env.get(secret) || "";
+
+    acc[secret] = value;
     return acc;
   }, {} as Record<Secret, string>);
+
+  const deploymentMode = values.DEPLOYMENT_MODE as DeploymentMode;
+
+  if (deploymentMode !== "local" && deploymentMode !== "production") {
+    throw new Error("DEPLOYMENT_MODE must be local or production");
+  }
+
+  if (deploymentMode === "local") {
+    const apiLocalPort = Deno.env.get("API_LOCAL_PORT");
+
+    if (!apiLocalPort) {
+      throw new Error("API_LOCAL_PORT not set");
+    }
+
+    return {
+      ...values,
+      DEPLOYMENT_MODE: deploymentMode,
+      API_LOCAL_PORT: apiLocalPort,
+    };
+  }
+
+  return {
+    ...values,
+    DEPLOYMENT_MODE: deploymentMode,
+  };
 }
-export const env = loadEnvVariables();
+
+export const env: Env = loadEnvVariables();
